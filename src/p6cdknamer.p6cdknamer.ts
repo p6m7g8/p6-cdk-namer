8678af2eec042eb83912cb22bec5c236645d0b38
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-17 09:40:50 -0500

    feat: handle Update and Delete

--- src/p6cdknamer.p6cdknamer.ts
+++ src/p6cdknamer.p6cdknamer.ts
@@ -1,6 +1,8 @@
 import type { CloudFormationCustomResourceEvent, CloudFormationCustomResourceResponse } from 'aws-lambda'
-import { CreateAccountAliasCommand, IAMClient } from '@aws-sdk/client-iam'
+import { CreateAccountAliasCommand, DeleteAccountAliasCommand, IAMClient } from '@aws-sdk/client-iam'
 import winston from 'winston'
+
+type Status = 'SUCCESS' | 'FAILED'
 
 const logger = winston.createLogger({
   level: 'info',
@@ -19,17 +21,51 @@ const iamClient = new IAMClient({})
  *
  * @param alias - The alias to create.
  */
-async function createAccountAlias(alias: string): Promise<void> {
+async function createAccountAlias(alias: string): Promise<Status> {
   const command = new CreateAccountAliasCommand({ AccountAlias: alias })
 
   try {
     const response = await iamClient.send(command)
     logger.info(`Successfully created account alias: ${alias}`, { response })
+    return 'SUCCESS'
   }
   catch (error) {
     logger.error(`Failed to create account alias: ${(error as Error).message}`)
-    throw error
+    return 'FAILED'
   }
+}
+
+/**
+ * Deletes an account alias for the AWS account.
+ *
+ * @param alias - The alias to create.
+ */
+async function deleteAccountAlias(alias: string): Promise<Status> {
+  const command = new DeleteAccountAliasCommand({ AccountAlias: alias })
+
+  try {
+    const response = await iamClient.send(command)
+    logger.info(`Successfully deleted account alias: ${alias}`, { response })
+    return 'SUCCESS'
+  }
+  catch (error) {
+    logger.error(`Failed to delete account alias: ${(error as Error).message}`)
+    return 'FAILED'
+  }
+}
+
+/**
+ * Updates an account alias for the AWS account.
+ *
+ * @param alias - The alias to create.
+ */
+async function updateAccountAlias(alias: string): Promise<Status> {
+  const deleteStatus = await deleteAccountAlias(alias)
+  if (deleteStatus !== 'SUCCESS') {
+    return deleteStatus
+  }
+  const createStatus = await createAccountAlias(alias)
+  return createStatus
 }
 
 /**
@@ -41,20 +77,23 @@ async function createAccountAlias(alias: string): Promise<void> {
 export async function handler(event: CloudFormationCustomResourceEvent): Promise<CloudFormationCustomResourceResponse> {
   logger.info(`Received event: ${JSON.stringify(event)}`)
 
-  const { ResourceProperties } = event
+  const { RequestType, ResourceProperties } = event
   const alias = ResourceProperties?.AccountAlias
 
-  try {
-    await createAccountAlias(alias)
-    logger.info(`Account alias creation succeeded for alias: ${alias}`)
+  let status: Status
+  if (RequestType === 'Delete') {
+    status = await deleteAccountAlias(alias)
   }
-  catch (error) {
-    logger.error(`Account alias creation failed: ${(error as Error).message}`)
-    throw error
+  else if (RequestType === 'Update') {
+    status = await updateAccountAlias(alias)
+  }
+  else {
+    status = await createAccountAlias(alias)
   }
 
   return {
-    Status: 'SUCCESS',
+    Status: status,
+    Reason: 'See the details in CloudWatch Log Stream',
     PhysicalResourceId: alias,
     StackId: event.StackId,
     RequestId: event.RequestId,
